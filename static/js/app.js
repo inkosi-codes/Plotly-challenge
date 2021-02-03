5065a7d8df76a83559a6911d6be12c8d4dc52723
unknown <kiwwanne@gmail.com>
2021-02-02 18:43:25 -0600

    Work on gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,7 @@ function getJSONData() {
         populateDemographic(data.metadata);
         showBarChart(data.samples);
         showBubbleChart(data.samples);
-        showguageChart();
+        showguageChart(data.metadata);
 
     });
 }
@@ -85,14 +85,15 @@ function showBubbleChart(bubbleData) {
 }
 
 
-function showguageChart() {
-    // var gauge_data = gaugeData.filter(data => data.id == sel_value);
+function showguageChart(gaugeData) {
+    var gauge_data = gaugeData.filter(data => data.id == sel_value);
+    console.log(gauge_data)
 
-    // var wfeq = gauge_data[0].wfeq;
+    var wfeq = gauge_data[0].wfeq;
     var trace = [{
         type: "pie",
         showlegend: false,
-        hole: 0.4,
+        hole: 0.2,
         rotation: 90,
         values: [180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
         text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
@@ -100,7 +101,7 @@ function showguageChart() {
         textinfo: "text",
         textposition: "inside",
         marker: {
-            colors: ['#F8F3EC','#F4F1E5','#E9E6CA','#E2E4B1','#D5E49D','#B7CC92','#8CBF88','#8ABB8F','#85B48A','white'],
+            colors: ['#F8F3EC','#F4F1E5','#E9E6CA','#E2E4B1','#D5E49D','#B7CC92','#8CBF88','#8ABB8F','#85B48A','#fff'],
         },
         labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9', ''],
         hoverinfo: "label"
@@ -116,15 +117,15 @@ function showguageChart() {
         shapes: [{
             type: 'line',
             x0: 0,
-            y0: 0,
+            y0: 3.5,
             x1: x,
             y1: 0.5,
             line: {
-                color: 'red',
+                color: '#b30000',
                 width: 4
             }
         }],
-        title: 'Belly Button Washing Frequency',
+        title: '<b>Belly Button Washing Frequency</b><br>Scrubs per Week',
         xaxis: { visible: false, range: [-1, 1] },
         yaxis: { visible: false, range: [-1, 1] }
     };
